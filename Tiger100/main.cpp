501b3f01f76c7d7b730bb73466e9771ed3c92eca
jzhangCSER01 <1760616780@qq.com>
2022-06-20 15:37:56 +0800

    initgraph() 创建窗口 solidrectangle() 这个函数用于画无边框的填充矩形

--- Tiger100/main.cpp
+++ Tiger100/main.cpp
@@ -6,6 +6,25 @@
 #include<easyx.h>
 int main()
 {
+	//创建窗口（绘制图形，图片...）
+	initgraph(760, 640);
 
+	/*
+	* solidrectangle 这个函数用于画无边框的填充矩形
+	* void solidrectangle(
+	*	int left,
+	*	int top,
+	*	int right,
+	*	int bottom
+	* );
+	* left 矩形左部 x 坐标
+	* top 矩形顶部 y 坐标
+	* right 矩形右部 x 坐标
+	* bottom 矩形底部 y 坐标
+	*/
+	//绘制图形
+	solidrectangle(10, 10, 10 + 20, 10 + 20);
+
+	getchar();
 	return 0;
 }
